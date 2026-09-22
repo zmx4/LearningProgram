97fe1de6683b14705ea3546a22f13e1ffd32648c
Tick Zhong <1601858426@qq.com>
2026-09-22 08:34:13 +0800

    项目配置

--- learning-program-backend/src/main/java/com/tick/config/SecurityConfiguration.java
+++ learning-program-backend/src/main/java/com/tick/config/SecurityConfiguration.java
@@ -1,4 +1,62 @@
 package com.tick.config;
 
+import com.tick.entity.RestBean;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.Nullable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import java.io.IOException;
+import java.security.Security;
+
+@Configuration
 public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(conf ->
+                        conf
+                                .requestMatchers("/api/auth/**").permitAll()
+                                .anyRequest().authenticated())
+                .formLogin(conf ->
+                        conf.loginProcessingUrl("/api/auth/login")
+                                .successHandler(this::onAuthenticationSuccess)
+                                .failureHandler(this::onAuthenticationFailure)
+                )
+                .logout(conf ->
+                        conf.logoutUrl("/api/auth/logout")
+                                .logoutSuccessHandler(this::onLogoutSuccess))
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(conf ->
+                        conf.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        response.setContentType("application/json");
+        response.getWriter().write(RestBean.success().asJsonString());
+    }
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+        response.setContentType("application/json");
+        response.getWriter().write(RestBean.failure(401,exception.getMessage()).asJsonString());
+    }
+    public void onLogoutSuccess(HttpServletRequest request,
+                                HttpServletResponse response,
+                                @Nullable Authentication authentication) throws IOException, ServletException {
+
+    }
 }
--- learning-program-backend/src/main/java/com/tick/entity/RestBean.java
+++ learning-program-backend/src/main/java/com/tick/entity/RestBean.java
@@ -1,4 +1,24 @@
 package com.tick.entity;
 
-public record RestBean() {
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONWriter;
+
+import java.io.Serializable;
+
+public record RestBean<T>(int code, T data, String message) implements Serializable {
+    public static <T>RestBean<T> success(T data) {
+        return new RestBean<T>(200, data, "success");
+    }
+
+    public static <T>RestBean<T> success() {
+        return success(null);
+    }
+
+    public static <T>RestBean<T> failure(int code, String message) {
+        return new RestBean<T>(code, null, message);
+    }
+
+    public String asJsonString() {
+        return JSONObject.toJSONString(this, JSONWriter.Feature.WriteNulls);
+    }
 }
