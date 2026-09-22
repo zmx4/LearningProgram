0b0759bb4b5821eb95b9da6571903438a9bd3ada
Tick Zhong <1601858426@qq.com>
2026-09-22 08:06:23 +0800

    初始化仓库

---
+++ learning-program-backend/src/main/java/com/tick/LearningProgramBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningProgramBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningProgramBackendApplication.class, args);
+    }
+
+}
---
+++ learning-program-backend/src/test/java/com/tick/LearningProgramBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningProgramBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
